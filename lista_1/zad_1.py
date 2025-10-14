59daff14b0cc4b314813ab1e285543550e891275
Kamil <kpawlik2005@gmail.com>
2025-10-14 08:32:40 +0200

    lista1

--- lista_1/zad_1.py
+++ lista_1/zad_1.py
@@ -2,6 +2,7 @@ import decimal
 from decimal import Decimal
 def vat_faktura(lista):
    return sum(lista) * 0.23
+
 def vat_paragon(lista):
     suma = 0
     for i in lista:
@@ -11,11 +12,14 @@ zakupy = [0.11, 0.33]
 print(vat_faktura(zakupy) == vat_paragon(zakupy))
 #
 zakupyd = [Decimal("0.11") , Decimal("0.33")]
+
 def vat_faktura_d(lista):
    return sum(lista) * Decimal("0.23")
+
 def vat_paragon_d(lista):
     suma = 0
     for i in lista:
         suma+= i *Decimal("0.23")
     return suma
+
 print(vat_faktura_d(zakupyd) == vat_paragon_d(zakupyd))
